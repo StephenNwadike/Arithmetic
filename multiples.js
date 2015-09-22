1a44966eed1a286fa82a90347675e16b186f2cc2
Stephen Nwadike <mank4cfc@gmail.com>
2015-09-22 21:55:50 +0100

    update multiples

---
+++ multiples.js
@@ -0,0 +1,16 @@
+var ans=0;
+
+for(var i=0; i<=1000; i++);
+if (i%3 === 0)
+{
+	ans+=i;
+	console.log("Sum of multiples of 3 =" + ans);
+	return ans;
+}
+else if (i%5 === 0)
+{
+	ans+=i;
+	Console.log("Sum of multiples of 3 and 5 =" + ans);
+	return ans;
+}
+else{ return ans; };
